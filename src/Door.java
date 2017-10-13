d84d6bab4a64ee85bd02bb4b796165b60496d80d
EmanMacario <emacario1997@hotmail.com>
2017-10-13 17:52:58 +1100

    Cleaned up Door class

--- src/Door.java
+++ src/Door.java
@@ -3,19 +3,37 @@ import org.newdawn.slick.SlickException;
 
 public class Door extends Sprite {
 	
+	/** Door attributes.
+	 */
 	private boolean closed;
 	
+	
+	/** Creates a new Door object. The
+	 * default is that the door is closed.
+	 * 
+	 * @param x  The x-coordinate of the sprite, in pixels.
+	 * @param y  The x-coordinate of the sprite, in pixels.
+	 */
 	public Door(float x, float y) {
 		super("res/door.png", x, y);
 		this.closed = true;
-		this.addTag("Door");
 	}
 	
 	
+	/** Returns if the door is currently closed.
+	 * 
+	 * @return True if closed else false.
+	 */
 	public boolean getClosed() {
 		return this.closed;
 	}
 	
+	
+	/** Opens or closes the door.
+	 * 
+	 * @param closed  True to close, false to open.
+	 * @return void
+	 */
 	public void setClosed(boolean closed) {
 		
 		/* Set door to open/closed. 
@@ -23,26 +41,26 @@ public class Door extends Sprite {
 		this.closed = closed;
 		
 		/* And add/remove "Blocked" tag
-		 * to indicate to Movable objects
-		 * that they cannot move through.
+		 * to indicate that Movable objects
+		 * cannot/can move through this location.
 		 */
-		if (!closed) {
+		if (!closed && compareTag("Blocked")) {
 			
-			if (this.compareTag("Blocked")) {
-				
-				System.out.println("Removed blocked tag");
-				this.removeTag("Blocked");
-			}
+			this.removeTag("Blocked");
 			
-		} else {
+		} else if (closed && !compareTag("Blocked")){
 			
-			if (!this.compareTag("Blocked")) {
-				System.out.println("Added blocked tag");
-				this.addTag("Blocked");
-			}
+			this.addTag("Blocked");
 		}
 	}
 	
+	
+	/** Only renders the door if the door is closed. 
+	 * 
+	 * @param g  The Slick graphics container object.
+	 * @throws SlickException
+	 * @return void
+	 */
 	@Override
 	public void render(Graphics g) throws SlickException {
 		if (!closed) {
